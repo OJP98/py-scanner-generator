26018b39e123237d148ffd4e2bc042f00ce17a0e
OJP98 <ojuarez_p@hotmail.com>
2021-04-08 23:39:03 -0600

    correclty parsing a set

--- set_parser.py
+++ set_parser.py
@@ -9,8 +9,8 @@ from utils import (
 )
 
 test_list = [Character('letter', ''), Character('symbol', '')]
-letter_ignore = ['(', '[', '{', '|']
-symbol_ignore = ['(', '[', '{', '|', ' ']
+symbol_ignore = ['(', '[', '{', '|']
+closing_symbols = ['{', '(', '[']
 
 
 class SetParser:
@@ -19,13 +19,16 @@ class SetParser:
         self.idents = idents
         self.curr_char = None
         self.prev_char = None
+        self.last_char = None
         self.Next()
 
     def Next(self):
         try:
             if self.curr_char == ' ' and self.prev_char == '|':
+                self.last_char = self.prev_char
                 self.prev_char = '.'
             else:
+                self.last_char = self.prev_char
                 self.prev_char = self.curr_char
 
             self.curr_char = next(self.set)
@@ -37,43 +40,38 @@ class SetParser:
 
             # curr_char is a letter
             if self.curr_char.isalpha():
-                if self.prev_char and self.prev_char not in letter_ignore:
+                if self.prev_char and self.prev_char not in symbol_ignore and self.last_char not in symbol_ignore:
                     yield Variable(VarType.APPEND)
                 yield self.GenerateWord()
 
             # curr_char is a char
             elif self.curr_char == '\'' or self.curr_char == '"':
-                if self.prev_char and self.prev_char not in letter_ignore:
+                if self.prev_char and self.prev_char not in symbol_ignore and self.last_char not in symbol_ignore:
                     yield Variable(VarType.APPEND)
                 yield self.GenerateVar(self.curr_char)
 
-            # curr_char is kleene expr (check prev_char)
-            elif self.curr_char == '{':
-                if self.prev_char and self.prev_char not in symbol_ignore:
+            # curr_char is a closing symbols
+            elif self.curr_char in closing_symbols:
+                if self.prev_char and self.prev_char not in symbol_ignore and self.last_char not in symbol_ignore:
                     yield Variable(VarType.APPEND)
+
+                if self.curr_char == '{':
+                    yield Variable(VarType.LKLEENE)
+                elif self.curr_char == '[':
+                    yield Variable(VarType.LBRACKET)
+                elif self.curr_char == '(':
+                    yield Variable(VarType.LPAR)
+
                 self.Next()
-                yield Variable(VarType.LKLEENE)
 
             # curr_char is kleene expr.
             elif self.curr_char == '}':
                 self.Next()
                 yield Variable(VarType.RKLEENE)
 
-            elif self.curr_char == '[':
-                if self.prev_char and self.prev_char not in symbol_ignore:
-                    yield Variable(VarType.APPEND)
-                self.Next()
-                yield Variable(VarType.LBRACKET)
-
             elif self.curr_char == ']':
                 self.Next()
                 yield Variable(VarType.RBRACKET)
-
-            elif self.curr_char == '(':
-                if self.prev_char and self.prev_char not in symbol_ignore:
-                    yield Variable(VarType.APPEND)
-                self.Next()
-                yield Variable(VarType.LPAR)
 
             elif self.curr_char == ')':
                 self.Next()
@@ -126,6 +124,6 @@ class SetParser:
 
 
 parser = SetParser(
-    "{ ['_'] letter symbol {letter} }", test_list)
+    "letter {letter} '.' {letter} ['E' ['+'|'-'] letter {letter}]", test_list)
 res = parser.Parse()
 print(list(res))
